25586a466fd6e881953c043b79d0d33368df7650
Hon Huin Chin <HOCHI@orsted.com>
2023-07-30 20:00:14 +0800

    Add display environ variable

--- CET_extractions.py
+++ CET_extractions.py
@@ -21,6 +21,8 @@ from flask import Flask, jsonify, request
 # document = Document("files\SCE3-2023-079_Corrected.docx")
 # files\SCE3-2023-094_Corrected.docx
 # files\SCE3-2023-079_Corrected.docx
+if os.environ.get('DISPLAY','') == '':
+    os.environ.__setitem__('DISPLAY', ':0.0')
 
 class CETExtraction():
     authors: Authors
